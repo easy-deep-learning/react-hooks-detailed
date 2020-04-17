8f35a645ed96ff9bd502c042af6dd10e816227f5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-17 10:27:45 +0000

    Deploy easy-deep-learning/react-hooks-detailed to github.com/easy-deep-learning/react-hooks-detailed.git:gh-pages

---
+++ precache-manifest.a16d2a32d95c5b5cff8dc0e187cd4aeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a7dc8140e409cee00be69208e9a268",
+    "url": "/react-hooks-detailed/index.html"
+  },
+  {
+    "revision": "f2cc954103f780facbf8",
+    "url": "/react-hooks-detailed/static/css/main.b1af7ae9.chunk.css"
+  },
+  {
+    "revision": "85a334fa5ad8ab472427",
+    "url": "/react-hooks-detailed/static/js/2.b2746927.chunk.js"
+  },
+  {
+    "revision": "f2cc954103f780facbf8",
+    "url": "/react-hooks-detailed/static/js/main.483c61bd.chunk.js"
+  },
+  {
+    "revision": "3eb61d1947839b51b687",
+    "url": "/react-hooks-detailed/static/js/runtime~main.c782ec43.js"
+  }
+]);
